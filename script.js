68fee393db0b61a69d9a012764413dcb99eff19f
Matheus Cruz <matheuscruz960@gmail.com>
2023-05-28 09:27:21 -0300

    melhorias e correção de bugs

--- script.js
+++ script.js
@@ -1,17 +1,4 @@
-const swiperBanner = new Swiper('.carrossel__banner', {
-    slidesPerView: 1,
-    speed:500,
-    keyboard:true,
-    pagination: {
-        el: '.swiper-pagination',
-        type: 'bullets',
-        },
-    autoplay: {
-        delay: 4000,
-        disableOnInteraction: false,
-        },
-    });
-const swiperVendidosMobile = new Swiper('.carrossel__vendidos__mobile', {
+const swiperVendidos = new Swiper('.swiper', {
     spaceBetween: 50,
     slidesPerView: 1,
     speed:300,
@@ -19,35 +6,13 @@ const swiperVendidosMobile = new Swiper('.carrossel__vendidos__mobile', {
     pagination: {
         el: '.swiper-pagination',
         type: 'bullets',
+    },
+    breakpoints: {
+        1000: {
+            spaceBetween: 550,
+            pagination: {
+                clickable: true,
+            },
         },
-    });
-const swiperLançamentos = new Swiper('.carrossel__lancamentos__mobile', {
-    spaceBetween: 50,
-    slidesPerView: 1,
-    speed:300,
-    keyboard:true,
-    pagination: {
-        el: '.swiper-pagination',
-        type: 'bullets',
-         },
-    });
-const swiperVendidosDesktop = new Swiper('.carrossel__vendidos__desktop', {
-    spaceBetween: 400,
-    slidesPerView: 1,
-    speed:300,
-    keyboard:true,
-    pagination: {
-        el: '.swiper-pagination',
-        type: 'bullets',
-        },
-    });
-const swiperLançamentosDesktop = new Swiper('.carrossel__lancamentos__desktop', {
-    spaceBetween: 400,
-    slidesPerView: 1,
-    speed:300,
-    keyboard:true,
-    pagination: {
-        el: '.swiper-pagination',
-        type: 'bullets',
-         },
-    });
+    },
+});
